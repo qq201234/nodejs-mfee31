f96c1bc811c3bdd1702480159d1f80dccbcbbc7d
Jun <qq201234go@gmail.com>
2022-12-03 15:24:30 +0800

    add hello&sum .js

---
+++ sum.js
@@ -0,0 +1,17 @@
+// function sum(n) {
+//     // 回傳 1+2+3+4+...+n 的結果
+//   }
+  
+//   console.log(sum(1)); // 1
+//   console.log(sum(2)); // 3
+//   console.log(sum(3)); // 6
+//   console.log(sum(10)); // 55
+
+function sum(n){
+    return n * (n+1) / 2
+}
+
+console.log(sum(1));
+console.log(sum(2));
+console.log(sum(3));
+console.log(sum(10));
