7afb87427277ab62a791b5ec6f5afc5e648a4cb3
Jun <qq201234go@gmail.com>
2022-12-17 12:03:33 +0800

    add .env

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -1,20 +1,24 @@
 // get the client
 const mysql2 = require('mysql2/promise');
 
+require('dotenv').config();
+
 (async () => {
     // create the connection to database
     const connection = await mysql2.createConnection({
-        host: '127.0.0.1',
-        port: 3306,
-        user: '',
-        password: '',
-        database: 'stock_mfee31',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME,
     });
 
     // simple query
-    let result = await connection.query('SELECT * FROM `stocks`');
-    let data = result[0];
+    // let result = await connection.query('SELECT * FROM `stocks`');
+    // let data = result[0];
     // console.log(result);
+    // 可簡寫
+    let [data, fields] = await connection.query('SELECT * FROM `stocks`');
     console.log(data);
 
     connection.end();
---
+++ crawler/readme.md
@@ -0,0 +1,9 @@
+# Crawler
+
+## Installation
+
+```bash
+cp .env.example .env
+```
+
+config your database username and password
